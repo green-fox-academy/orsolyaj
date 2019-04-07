a7efd1491947eebccfa1c21ee29f63ceaa95a953
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-04-07 22:59:26 +0200

    DrawDiagonal excercise

--- week-01/day-5/src/DrawDiagonal.java
+++ week-01/day-5/src/DrawDiagonal.java
@@ -1,5 +1,19 @@
 public class DrawDiagonal {
   public static void main(String[] args){
+    int n = 10;
+    int i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+      for (j = 1; j <= n; j++) {
+        if (i == 1 || i == n || j == 1 || j == n || i == j)
+          System.out.print("*");
+        else
+          System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
+  }
 
   }
-}
+
