d42d3ccf6ff54df93ce5d3b5f7995759953a02b4
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-04-07 23:38:42 +0200

    DrawChessTable excercies

--- week-01/day-5/src/DrawChessTable.java
+++ week-01/day-5/src/DrawChessTable.java
@@ -1,9 +1,18 @@
 public class DrawChessTable {
   public static void main(String[] args) {
-    int imp = 8;
-
-
-
+    int n = 8;
+    for (int r = 1; r <= n; r++ ) {
+      for (int c = 1; c <= n; c++) {
+        if ( r % 2 == 1  && c % 2 == 1 ) {
+          System.out.print("%");
+        } else if (r % 2 == 0  && c % 2 == 0) {
+          System.out.print("%");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.print("\n");
+    }
     }
   }
 
