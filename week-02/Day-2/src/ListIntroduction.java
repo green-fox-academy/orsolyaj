8bbbb02f877a241622beab670ffdb406ca8457dc
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-04-09 16:37:49 +0200

    HashMap excercise

--- week-02/Day-2/src/ListIntroduction.java
+++ week-02/Day-2/src/ListIntroduction.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.ListIterator;
 
 public class ListIntroduction {
-  public static void main(String[] args){
-    ArrayList <String> list = new ArrayList<>();
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
     System.out.println(list.size());
     list.add("William");
     System.out.println(list.isEmpty());
@@ -13,10 +13,10 @@ public class ListIntroduction {
     list.add("Amanda");
     System.out.println(list.size());
     System.out.println(list.get(2));
-    for (String name : list){
+    for (String name : list) {
       System.out.println(name);
     }
-    Iterator <String> listIterator = list.iterator();
+    Iterator<String> listIterator = list.iterator();
     while (listIterator.hasNext()) {
       System.out.println(listIterator.next());
     }
