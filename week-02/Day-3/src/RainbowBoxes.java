c2a50566356119f311955096d81804ab690fc7f8
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-04-10 18:19:05 +0200

    Colored exc

--- week-02/Day-3/src/RainbowBoxes.java
+++ week-02/Day-3/src/RainbowBoxes.java
@@ -1,32 +1,33 @@
-import java.util.Scanner;
-
 import javax.swing.*;
-
+import java.util.Arrays;
+import java.util.List;
 import java.awt.*;
+import java.util.ArrayList;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Horizontal {
+public class RainbowBoxes {
   public static void mainDraw(Graphics graphics) {
-    Scanner sc = new Scanner(System.in);
-    for (int i = 0; i < 3; ++i) {
-      System.out.println("Enter parameters:");
-      int par1 = sc.nextInt();
-      int par2 = sc.nextInt();
-      LineDraw(par1, par2, graphics);
+    int a = 320;
+    List<Color> unicorn = new ArrayList<>(Arrays.asList
+            (Color.yellow, Color.orange, Color.red, Color.pink, Color.blue, Color.blue, Color.red));
+
+    int color = 0;
+    for (int i = 0; i < 10; i++) {
+
+      if (color == unicorn.size()) {
+        color = 0;
+      }
+      sqDr(a - (i * 32), graphics, unicorn.get(color));
+      ++color;
     }
   }
 
-  public static void LineDraw(int x, int y, Graphics graphics) {
-      graphics.setColor(Color.BLACK);
-      graphics.drawLine(x, y, x + 50, y);
-    }
-
-
-    // Create a line drawing function that takes 3 parameters:
-    // The x and y coordinates of the line's starting point and the graphics
-    // and draws a 50 long horizontal line from that point.
-    // Draw 3 lines with that function. Use loop for that.
+  public static void sqDr(int a, Graphics graphics, Color c) {
+    graphics.setColor(c);
+    graphics.fillRect(WIDTH / 2 - a / 2, HEIGHT / 2 - a / 2, a, a);
+  }
+  
   // Don't touch the code below
   static int WIDTH = 320;
   static int HEIGHT = 320;
@@ -49,4 +50,5 @@ public class Horizontal {
       mainDraw(graphics);
     }
   }
+
 }
