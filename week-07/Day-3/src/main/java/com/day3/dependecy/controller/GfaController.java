8fbc6f343262624308a5681bf562ca124f1eae1b
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-05-22 18:17:04 +0200

    gfa exc, except last task

--- week-07/Day-3/src/main/java/com/day3/dependecy/controller/GfaController.java
+++ week-07/Day-3/src/main/java/com/day3/dependecy/controller/GfaController.java
@@ -1,4 +1,50 @@
 package com.day3.dependecy.controller;
 
+import com.day3.dependecy.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class GfaController {
+  @Autowired
+  @Qualifier("studentService")
+  StudentService studentService;
+
+  @GetMapping("/gfa")
+  public String homePage(Model model) {
+    model.addAttribute("count",studentService.counter());
+    return "gfa";
+  }
+
+  @GetMapping("/gfa/list")
+  public String listPage(Model model) {
+    model.addAttribute("student", studentService);
+    return "gfalist";
+  }
+
+  @GetMapping("/gfa/form")
+  public String showForm() {
+    return "gfaform";
+  }
+
+  @PostMapping("/gfa/form")
+  public String addStudent(String name) {
+    studentService.save(name);
+    return "redirect:/gfa/list";
+  }
+
+  @GetMapping("/gfa/check")
+  public String nameCheck() {
+    return "namecheckform";
+  }
+
+  @PostMapping("/gfa/check")
+  public String namecheckin(String name, Model model) {
+    model.addAttribute("text", studentService.containsIt(name));
+    return "redirect:/gfa/list";
+  }
 }
---
+++ week-07/Day-3/src/main/java/com/day3/dependecy/service/StudentService.java
@@ -0,0 +1,40 @@
+package com.day3.dependecy.service;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Qualifier("studentService")
+public class StudentService {
+  ArrayList<String> names;
+
+  public StudentService() {
+    names = new ArrayList<>();
+    names.add("Sanyi");
+    names.add("Lilla");
+    names.add("John");
+    names.add("jani");
+  }
+
+  public List<String> findAll() {
+    return names;
+  }
+
+  public void save(String student) {
+    names.add(student);
+  }
+
+  public  int counter() {
+    return names.size();
+  }
+
+  public String containsIt(String name) {
+    if(name.contains(name)) {
+      return "list contains name";
+    } else return "list doesnt contain name";
+  }
+
+}
